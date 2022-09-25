fea1043588faffa49c9c4529fb89bd4e44bc6922
Dishant <dishant.rathee@gmail.com>
2022-09-25 12:56:16 +0530

    queue

--- js/script.js
+++ js/script.js
@@ -136,7 +136,6 @@ var q_top='',q_next='';
 joinBtnGuest.addEventListener('click',async()=>{
   username=document.getElementById("name").value;
   const response = await fetch('https://mytestsite.net.in/enqueue',{
-    mode:'no-cors',
     method:'POST',
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({'user':username})
@@ -230,9 +229,7 @@ var ele;
 
 async function renderPeers(peers) {
 
-  const response = await fetch('https://mytestsite.net.in/getQueue',{
-    mode:'no-cors'
-  });
+  const response = await fetch('https://mytestsite.net.in/getQueue');
   const resp_data=await response.json();
 
   q=resp_data.queue;
