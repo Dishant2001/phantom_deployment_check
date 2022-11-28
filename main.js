c5aa1c632dc2c1ffbf21f688615f3cb2ff20965b
Dishant <dishant.rathee@gmail.com>
2022-11-28 15:09:54 +0530

    chat-implemented

--- main.js
+++ main.js
@@ -261,7 +261,7 @@ async function setBackgroundImage() {
     if (!virtualBackgroundEnabled) {
         imgElement = document.createElement('img');
 
-        imgElement.src = 'https://raw.githubusercontent.com/Dishant2001/phantom_deployment_check/using-Agora-SDK/background.jpg' + new Date().getTime();
+        imgElement.src = 'https://raw.githubusercontent.com/Dishant2001/phantom_deployment_check/using-Agora-SDK/background.jpg/' + new Date().getTime();
         imgElement.setAttribute('crossOrigin', '');
 
         let processor = await getProcessorInstance();
